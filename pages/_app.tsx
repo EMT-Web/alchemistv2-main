588459336d9a1a9a3e8ae2d05444804d46aee437
Rana-Hassan7272 <ssc.shahbaz.2004@gmail.com>
2025-10-30 18:48:30 +0500

    first commithss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
+import CookieBanner from '../components/CookieBanner';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -47,6 +48,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
             <Analytics />
             <Footer/>
+            <CookieBanner />
             
             {/* <div id="ftco-loader" className="show fullscreen"><svg className="circular" width="48px" height="48px"><circle className="path-bg" cx="24" cy="24" r="22" fill="none" strokeWidth="4" stroke="#eeeeee"/><circle className="path" cx="24" cy="24" r="22" fill="none" strokeWidth="4" strokeMiterlimit="10" stroke="#F96D00"/></svg></div> */}
             
